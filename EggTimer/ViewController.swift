6da280a527c8314761eba479121821e552ac30f5
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2024-04-23 11:54:00 +0530

    Egg timer done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,17 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var barProgressView: UIProgressView!
     
     let eggTimes = ["Soft" : 3, "Medium": 4, "Hard": 7]
     
     var secondsRemaining : Int = 60
+    var totalSeconds : Int = 60
     var timer : Timer?
     
     @IBAction func onHardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         secondsRemaining = eggTimes[hardness]!
+        totalSeconds = eggTimes[hardness]!
         
         timer?.invalidate()
+        
+        barProgressView.progress = 0.0
+        titleLabel.text = "One \(hardness) boiled egg, coming up!"
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
@@ -30,9 +36,18 @@ class ViewController: UIViewController {
         if(secondsRemaining > 0) {
             print(secondsRemaining)
             secondsRemaining -= 1
-        } else {
-            titleLabel.text = "DONE!"
-            timer!.invalidate()
+            let percentage = 1.0 - Float(secondsRemaining) / Float(totalSeconds)
+            
+            UIView.animate(withDuration: 0.2) {
+                self.barProgressView.progress = percentage
+                self.view.layoutIfNeeded()
+            }
+            
+            if secondsRemaining == 0 {
+                titleLabel.text = "DONE!"
+                timer!.invalidate()
+            }
+            
         }
     }
     
