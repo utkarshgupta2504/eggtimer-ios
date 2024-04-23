66d93be1cc91ee86623f3fb9c2da77f66226dab4
Utkarsh Gupta <52123629+utkarshgupta2504@users.noreply.github.com>
2024-04-23 08:54:10 +0530

    Implemented dictionary

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes = ["Soft" : 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func onHardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let eggTime = eggTimes[hardness]!
+    }
+    
 
 }
